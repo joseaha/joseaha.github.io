57313d5bb8251bb6182db4fe5f5cda6969808132
Jose Hernandez <122076475+joseaha@users.noreply.github.com>
2026-03-29 12:18:49 -0600

    Feat: Upadate ui y ux

--- JS/index.js
+++ JS/index.js
@@ -20,29 +20,6 @@ function handleClickNavItem() {
     checkNavbar.checked = !checkNavbar.checked;
 }
 
-var anchoVentana = window.innerWidth;
-
-window.onresize = function () {
-    anchoVentana = window.innerWidth;
-    console.log(anchoVentana);
-    ajustarClasesContenedorFoto();
-};
-
-function ajustarClasesContenedorFoto() {
-    const contenedor_foto = document.querySelector('#contenedor_foto_perfil');
-
-    if (anchoVentana < 986) {
-        contenedor_foto.classList.remove('align-self-end', 'justify-content-end', 'me-5', 'm-5');
-        contenedor_foto.classList.add('align-self-center', 'justify-content-center', 'mb-5');
-    }
-    else if (anchoVentana > 986) {
-        contenedor_foto.classList.remove('align-self-center', 'justify-content-center', 'mb-5');
-        contenedor_foto.classList.add('align-self-end', 'justify-content-end', 'me-5');
-    }
-}
-
-ajustarClasesContenedorFoto();
-
 let slider = document.querySelector(".slider")
 let currentSlide = 0;
 let totalSlides = slider.querySelectorAll(".wrapper .left > div").length - 1;
